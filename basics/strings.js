ffcbe33a3d7a3cd97fc70b5b131d13246846ea67
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-02-09 16:00:14 -0600

    strings exercise

---
+++ basics/strings.js
@@ -0,0 +1,13 @@
+let name = "Robert Young";
+
+console.log("String: " + name);
+
+let firstName = "Robert";
+let lastName = "Young";
+
+console.log(`Shorthand method to write full name: ${firstName} ${lastName}`);
+
+// OR
+let fullName = firstName + " " + lastName;
+
+console.log("Concat method to write full name: " + fullName)
