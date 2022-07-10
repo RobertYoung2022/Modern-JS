972b5fbbb579955c599fd92c495a913592c25cea
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-07-09 21:41:14 -0500

    created temperature conversion calculator

---
+++ basics/temp-conversion.js
@@ -0,0 +1,12 @@
+let fahrenheit = 32;
+
+// calculate celsius and store in celsius variable
+// print that value
+let celsius = (fahrenheit - 32) / 1.8;
+console.log(celsius);
+
+// calculate kelvin value and store in variable\
+// print that value
+let kelvin = (fahrenheit - 32) / 1.8 + 273.15;
+console.log(kelvin);
+
---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+// rule 1. you cannot define a variable more than once
+
+let petName = 'Blaze';
+// let petName = 'max'; // ex. for rule 1
+petName = 'Max'; // you can only reassign a variable like this
+
+console.log(petName);
+
+// rule 2. there are rules related to variables names
+//  let 3 = 2; ex. for rule 2 - cannot use a number or symbols first for variable
+let result = 3+ 4;
+
+// rule 3. variable names cannot be reserved keywords
+// let let = 12 ex. for rule 3
